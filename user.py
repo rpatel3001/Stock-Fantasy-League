64aa0b0cff755d11f967ca3d6dfa590735fae503
Oz Bejerano <ozbejerano@gmail.com>
2018-02-16 12:44:58 -0500

    Added User - 1

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    highestUID = 0
+    def createLeague(self,leagueName, startBalance,numberOfDays,restrcitions):
+        # add constructor for create League
+        #League(leagueName,
+    def joinLeague(self, leagueName,startBalance,numberOfDays):
+        #add code to join league
+
+    def __init__(self,UID,userPreferences,lastUsedPlayer,playersArray,totalPoints,mostRecentAcheievements,playerAchievements):
+        self.uid = ++highestUID
+        self.userPreferences = Preferences()
+        self.layers_array = None
+        self.total_points = None
+        self.mostRecentAchievements = None
+        self.player_Achievements = None
