0c8b736a0b94ee824971b4d9bbe0f56c1d5ff589
yaooo <yaoshi616@gmail.com>
2018-02-17 15:29:31 -0500

    Add more code

--- UserTeam/Player.py
+++ UserTeam/Player.py
@@ -1,8 +1,37 @@
-import numpy as np
-
 '''
     TODO I don't think we need limitBuy and limitSell
 '''
+
+
+# For one stock, the stock info is stored in the following class
+class stockInfo:
+    def __init__(self, stockTicker, shares):
+        self.stockTicker = stockTicker
+        self.shares = shares
+
+    # Return how many shares of a stock this person is holding
+    def getShares(self):
+        return self.holdings
+
+
+class StockHolding:
+    def __init__(self):
+        # holds is an array of stocks a player is holding right now
+        self.holds = []
+
+    def buyStock(self, stockTicker, shares):
+        # TODO add more conditions
+        self.holds.append(stockInfo(stockTicker, shares))
+
+    def sellStock(self, stockTicker, shares):
+        holds = self.holds
+        for i in holds:
+            if i.stockTicker == stockTicker:
+                # TODO add more conditions
+                return 0
+            else:
+                print("The player does not own the stock selected.")
+                return -1
 
 
 class Player:
--- user.py
+++ user.py
@@ -1,20 +1,16 @@
 from preferences import *
 
-
 class User:
     highestUID = 0
-
-    def createLeague(self, leagueName, startBalance, numberOfDays, restrictions):
+    def createLeague(self,leagueName, startBalance,numberOfDays,restrictions):
         # add constructor for create League
         return
         # League(leagueName,startBalance,numberOfDays,restrictions)
-
-    def joinLeague(self, leagueName, startBalance, numberOfDays):
-        # add code to join league
+    def joinLeague(self, leagueName,startBalance,numberOfDays):
+        #add code to join league
         return
 
-    def __init__(self, UID, userPreferences, lastUsedPlayer, playersArray, totalPoints, mostRecentAcheievements,
-                 playerAchievements):
+    def __init__(self,UID,userPreferences,lastUsedPlayer,playersArray,totalPoints,mostRecentAcheievements,playerAchievements):
         self.uid = UID
         self.userPreferences = Preferences()
         self.layers_array = None
