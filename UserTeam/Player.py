951fd9b63ad9d60440c39f8f771b09f0389eb61c
yaooo <yaoshi616@gmail.com>
2018-02-17 14:48:40 -0500

    Setting up the code. Not implemented yet because I am not too sure about the data structures we will use. Will think about it before coding

---
+++ UserTeam/Player.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+'''
+    TODO I don't think we need limitBuy and limitSell
+'''
+
+
+class Player:
+    def __init__(self, holding, fund=5000):
+        self.holding = holding
+        self.fund = fund
+
+    def getHolding(self):
+        return self.holding
+
+    def marketBuy(self, sotckTicker, unitPrice, amount):
+        if amount * unitPrice < self.fund:
+            print("The player does not have enough money")
+            return -1
+        else:
+    # TODO add the stock information into the field of holding
+        return 0
+
+    def marketSell(self, stockTicker, unitPrice, amount):
+
+    # TODO if the player does not hold enough shares
+
+
+    def limitBuy(self, stockTicker, amount, limitPrice):
+        if amount * limitPrice < self.fund:
+            print("The player does not have enough money")
+            return -1
+        else:
+        # TODO add the stock information into the field of holding
+        return 0
+
+    def limitSell(self, stockTicker, amount, limitPrice):
+
+    # TODO
+
+    def shortBuy(self, stockTicker, amount, price):
+
+    # TODO
+
+    def shortSell(self, stockTicker, amount, price):
+
+    # TODO
