3a48ff8d77f2960a1121339758b60951065225b2
Rajan Patel <rpatel3001@gmail.com>
2018-02-19 22:32:18 -0500

    stock data can pull current price on one ticker

--- StockData.py
+++ StockData.py
@@ -1,7 +1,24 @@
-getCurrentPrice(ticker)
+from requests import get
 
-getCurrentPrices(tickers)
+_AV_URL = "https://www.alphavantage.co/query"
 
-getPriceHistory(ticker, start, end, resolution)
+def getCurrentPrice(ticker):
+	args = {"function" : "TIME_SERIES_INTRADAY",
+			"symbol" : ticker.upper(),
+			"interval" : "1min",
+			"apikey" : "DEDSQFY460FDRASD"}
+	data_json = get(_AV_URL, params=args).json()
+	if list(data_json.keys())[0] == 'Error Message':
+		return data_json
+	else:
+		data = data_json['Time Series (1min)']
+		return data[list(data.keys())[0]]['4. close']
 
-getTechnicalIndicator(ticker, indicator, resolution, period)
+def getCurrentPrices(tickers):
+	pass
+
+def getPriceHistory(ticker, start, end, resolution):
+	pass
+
+def getTechnicalIndicator(ticker, indicator, resolution, period):
+	pass
