cdeb9f13971e13ddee0a6df95f054c1ded78fc12
Rajan Patel <rpatel3001@gmail.com>
2018-02-22 16:06:39 -0500

    get price data with batch quote instead of detailed quote

--- StockData.py
+++ StockData.py
@@ -3,16 +3,15 @@ from requests import get
 _AV_URL = "https://www.alphavantage.co/query"
 
 def getCurrentPrice(ticker):
-	args = {"function" : "TIME_SERIES_INTRADAY",
-			"symbol" : ticker.upper(),
-			"interval" : "1min",
+	args = {"function" : "BATCH_STOCK_QUOTES",
+			"symbols" : [ticker.upper()],
 			"apikey" : "DEDSQFY460FDRASD"}
 	data_json = get(_AV_URL, params=args).json()
 	if list(data_json.keys())[0] == 'Error Message':
 		return data_json
 	else:
-		data = data_json['Time Series (1min)']
-		return data[list(data.keys())[0]]['4. close']
+		data = list(data_json['Stock Quotes'])[0]
+		return data['2. price']
 
 def getCurrentPrices(tickers):
 	pass
