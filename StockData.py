2f18c0f39e0c83697f1aa3a243a0952c08310579
Rajan Patel <rpatel3001@gmail.com>
2018-02-22 16:23:28 -0500

    fill in function to pull multiple tickers at once

--- StockData.py
+++ StockData.py
@@ -14,7 +14,16 @@ def getCurrentPrice(ticker):
 		return data['2. price']
 
 def getCurrentPrices(tickers):
-	pass
+	tickers = [x.upper() for x in tickers]
+	args = {"function" : "BATCH_STOCK_QUOTES",
+			"symbols" : ','.join(tickers),
+			"apikey" : "DEDSQFY460FDRASD"}
+	data_json = get(_AV_URL, params=args).json()
+	if list(data_json.keys())[0] == 'Error Message':
+		return data_json
+	else:
+		data = list(data_json['Stock Quotes'])
+		return dict(zip(tickers, [x['2. price'] for x in data]))
 
 def getPriceHistory(ticker, start, end, resolution):
 	pass
