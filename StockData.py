360e8b296aa1b9470a4f58d6e3efb8ad17e1ba0b
Rajan Patel <rpatel3001@gmail.com>
2018-02-24 22:52:37 -0500

    restyle my files to follow PEP8 style guidelines

--- StockData.py
+++ StockData.py
@@ -1,32 +1,59 @@
+"""
+Provide data about securities that are tradeable on the fantasy platform.
+
+This includes price data for various time periods at various intervals
+for stocks, ETFs, and cryptocurrencies.
+"""
+
 from requests import get
 
 _AV_URL = "https://www.alphavantage.co/query"
 
-def getCurrentPrice(ticker):
-	args = {"function" : "BATCH_STOCK_QUOTES",
-			"symbols" : [ticker.upper()],
-			"apikey" : "DEDSQFY460FDRASD"}
-	data_json = get(_AV_URL, params=args).json()
-	if list(data_json.keys())[0] == 'Error Message':
-		return data_json
-	else:
-		data = list(data_json['Stock Quotes'])[0]
-		return data['2. price']
 
-def getCurrentPrices(tickers):
-	tickers = [x.upper() for x in tickers]
-	args = {"function" : "BATCH_STOCK_QUOTES",
-			"symbols" : ','.join(tickers),
-			"apikey" : "DEDSQFY460FDRASD"}
-	data_json = get(_AV_URL, params=args).json()
-	if list(data_json.keys())[0] == 'Error Message':
-		return data_json
-	else:
-		data = list(data_json['Stock Quotes'])
-		return dict(zip(tickers, [x['2. price'] for x in data]))
+def get_current_price(ticker):
+	"""
+	Gets the current price of a security.
+	"""
 
-def getPriceHistory(ticker, start, end, resolution):
-	pass
+    args = {"function": "BATCH_STOCK_QUOTES",
+            "symbols": [ticker.upper()],
+            "apikey": "DEDSQFY460FDRASD"}
+    data_json = get(_AV_URL, params=args).json()
+    if list(data_json.keys())[0] == 'Error Message':
+        return data_json
+    else:
+        data = list(data_json['Stock Quotes'])[0]
+        return data['2. price']
 
-def getTechnicalIndicator(ticker, indicator, resolution, period):
-	pass
+
+def get_current_prices(tickers):
+	"""
+	Get the current price of each security in a list.
+	"""
+
+    tickers = [x.upper() for x in tickers]
+    args = {"function": "BATCH_STOCK_QUOTES",
+            "symbols": ','.join(tickers),
+            "apikey": "DEDSQFY460FDRASD"}
+    data_json = get(_AV_URL, params=args).json()
+    if list(data_json.keys())[0] == 'Error Message':
+        return data_json
+    else:
+        data = list(data_json['Stock Quotes'])
+        return dict(zip(tickers, [x['2. price'] for x in data]))
+
+
+def get_price_history(ticker, start, end, resolution):
+	"""
+	Get the price history of a security.
+	"""
+
+    pass
+
+
+def get_technical_indicator(ticker, indicator, resolution, period):
+	"""
+	Get a technical indicator for a security.
+	"""
+
+    pass
--- main.py
+++ main.py
@@ -1,36 +1,57 @@
+"""
+Main server appplication for the fantasy platform.
+
+Provides routing for dynamic parts of the site, and links front-end API calls
+to back-end database transactions.
+"""
+
+import StockData
 from flask import Flask
 import pyrebase
-import StockData
 
 app = Flask(__name__)
 
 apikeyfile = open('apikey.txt', 'r')
 apikey = apikeyfile.readline()
 fbconfig = {
-	"apiKey": apikey,
-	"authDomain": "stock-fantasy-league.firebaseapp.com",
-	"databaseURL": "https://stock-fantasy-league.firebaseio.com",
-	"storageBucket": "stock-fantasy-league.appspot.com",
-	"serviceAccount": "serviceaccountdetails.json"
+    "apiKey": apikey,
+    "authDomain": "stock-fantasy-league.firebaseapp.com",
+    "databaseURL": "https://stock-fantasy-league.firebaseio.com",
+    "storageBucket": "stock-fantasy-league.appspot.com",
+    "serviceAccount": "serviceaccountdetails.json"
 }
 firebase = pyrebase.initialize_app(fbconfig)
 auth = firebase.auth()
 db = firebase.database()
 
+
 @app.route('/')
-def getstock():
-	user = db.child("users").child("u1").get().val()
-	return user['firstname'] + " has picked " + user['stock'] + " which costs $" + StockData.getCurrentPrice(user['stock'])
+def get_stock():
+	"""
+	Landing page.
+
+	Shows stored stock and it's current price.
+	"""
+
+    user = db.child("users").child("u1").get().val()
+    return user['firstname'] + " has picked " + user['stock'] \
+        + " which costs $" + StockData.getCurrentPrice(user['stock'])
+
 
 @app.route('/<string:user>/<string:stock>')
-def setstock(user, stock):
-	users = db.child("users").get()
-	for u in users.each():
-		if u.val()['firstname'] == user:
-			db.child("users").child(u.key()).update({"stock":stock})
-			return "updated " + u.val()['firstname'] + "'s stock to " + stock
+def set_stock(user, stock):
+	"""
+	Updates stored stock.
+	"""
+	
+    users = db.child("users").get()
+    for u in users.each():
+        if u.val()['firstname'] == user:
+            db.child("users").child(u.key()).update({"stock": stock})
+            return "updated " + u.val()['firstname'] + "'s stock to " + stock
 
-	return "user not found"
+    return "user not found"
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
