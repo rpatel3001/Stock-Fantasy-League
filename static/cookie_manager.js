b8975de781d061590737b1de51fe6ed698dbb671
Brian <bgm31@scarletmail.rutgers.edu>
2018-03-24 16:48:38 -0400

    commit

---
+++ static/cookie_manager.js
@@ -0,0 +1,31 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+          var scope = angular.element($("#signedInDropdown")).scope();
+    scope.$apply(function(){
+        scope.signedIn = true;
+        scope.username = profile.getName();
+        scope.imageurl = profile.getImageUrl();
+    });
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+      var scope = angular.element($("#signedInDropdown")).scope();
+    auth2.signOut().then(function () {
+            scope.$apply(function(){
+                        scope.username = '';
+        scope.imageurl = '';
+        scope.signedIn = false;
+    });
+      console.log('User signed out.');
+    });
+  }
+function wantSignIn(){
+    angular.element($("#loginView")).scope();
+    scope.$apply(function(){
+        scope.showLoginIn = true;
+    });
+}
