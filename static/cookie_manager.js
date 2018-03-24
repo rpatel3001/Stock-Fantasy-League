43831f1edf44cf0ead084a75217d280457d0236e
Oz Bejerano <ozbejerano@gmail.com>
2018-03-24 14:26:23 -0400

    Oz Front End 3/24 4test16

--- static/cookie_manager.js
+++ static/cookie_manager.js
@@ -1,4 +1,3 @@
-var r_scope = $rootscope;
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
