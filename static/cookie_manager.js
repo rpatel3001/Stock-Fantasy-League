5257ea41b2b614afdf561337bd7afddf103ed493
Oz Bejerano <ozbejerano@gmail.com>
2018-03-24 05:43:26 -0400

    Oz Front End 3/24 4test5

--- static/cookie_manager.js
+++ static/cookie_manager.js
@@ -6,3 +6,39 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + user_profile.getImageUrl());
   console.log('Email: ' + user_profile.getEmail()); // This is null if the 'email' scope is not present.
 }*/
+/**
+ * The Sign-In client object.
+ */
+var auth2;
+
+/**
+ * Initializes the Sign-In client.
+ */
+var initClient = function() {
+    gapi.load('auth2', function(){
+        /**
+         * Retrieve the singleton for the GoogleAuth library and set up the
+         * client.
+         */
+        auth2 = gapi.auth2.init({
+            client_id: 'CLIENT_ID.apps.googleusercontent.com'
+        });
+
+        // Attach the click handler to the sign-in button
+        auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
+    });
+};
+
+/**
+ * Handle successful sign-ins.
+ */
+var onSuccess = function(user,$rootscope) {
+    console.log('Signed in as ' + user.getBasicProfile().getName());
+ };
+
+/**
+ * Handle sign-in failures.
+ */
+var onFailure = function(error) {
+    console.log(error);
+};
