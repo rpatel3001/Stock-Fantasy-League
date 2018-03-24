e4cf3ee5914aca61b6365a631f3e001229bc5f44
Oz Bejerano <ozbejerano@gmail.com>
2018-03-24 14:46:27 -0400

    Oz Front End 3/24 4test19

--- static/cookie_manager.js
+++ static/cookie_manager.js
@@ -1,13 +1,16 @@
+var signedIn = false;
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
+    signedIn =true;
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
-  function signOut() {
+  /*function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
+        signedIn = false;
       console.log('User signed out.');
     });
-  }
+  }*/
--- static/main.js
+++ static/main.js
@@ -85,6 +85,12 @@ stk.controller('LeagueController', function($scope){
     };
 });
         stk.controller('NavbarController',['$scope', function($scope){
+                    $scope.signOut = function(){
+            console.log("sign out cool");
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut.then(function(){
+                console.log('userSignedOut');
+            });
             $scope.$on('login',function(events,args){
                 var user_profile = googleUser.getBasicProfile();
                 $scope.username = user_profile.getName();
