4732d581a4fb480e0f4753976a5b8a717f92e4e1
Oz Bejerano <ozbejerano@gmail.com>
2018-03-24 06:24:02 -0400

    Oz Front End 3/24 4test12

--- static/cookie_manager.js
+++ static/cookie_manager.js
@@ -2,93 +2,12 @@ var auth2; // The Sign-In object.
 var googleUser; // The current user.
 var r_scope = $rootscope;
 
-/**
- * Calls startAuth after Sign in V2 finishes setting up.
- */
-var appStart = function() {
-  gapi.load('auth2', initSigninV2);
-};
-
-/**
- * Initializes Signin v2 and sets up listeners.
- */
-var initSigninV2 = function(r_scope) {
-  auth2 = gapi.auth2.init({
-      client_id: '667783900886-6p5fn2lrhu0q265ibuuufmk3t95ack93.apps.googleusercontent.com',
-      scope: 'profile'
-  });
-
-  // Listen for sign-in state changes.
-  auth2.isSignedIn.listen(signinChanged);
-
-  // Listen for changes to current user.
-  auth2.currentUser.listen(userChanged);
-
-  // Sign in the user if they are currently signed in.
-  if (auth2.isSignedIn.get() == true) {
-    auth2.signIn();
-  }
-
-  // Start with the current live values.
-  refreshValues();
-r_scope.$broadcast('login');
-};
-
-/**
- * Listener method for sign-out live value.
- *
- * @param {boolean} val the updated signed out state.
- */
-var signinChanged = function (val) {
-  console.log('Signin state changed to ', val);
-  document.getElementById('signed-in-cell').innerText = val;
-};
-
-/**
- * Listener method for when the user changes.
- *
- * @param {GoogleUser} user the updated user.
- */
-var userChanged = function (user) {
-  console.log('User now: ', user);
-  googleUser = user;
-  updateGoogleUser();
-  document.getElementById('curr-user-cell').innerText =
-    JSON.stringify(user, undefined, 2);
-};
-
-/**
- * Updates the properties in the Google User table using the current user.
- */
-var updateGoogleUser = function () {
-  if (googleUser) {
-    document.getElementById('user-id').innerText = googleUser.getId();
-    document.getElementById('user-scopes').innerText =
-      googleUser.getGrantedScopes();
-    document.getElementById('auth-response').innerText =
-      JSON.stringify(googleUser.getAuthResponse(), undefined, 2);
-  } else {
-    document.getElementById('user-id').innerText = '--';
-    document.getElementById('user-scopes').innerText = '--';
-    document.getElementById('auth-response').innerText = '--';
-  }
-};
-
-/**
- * Retrieves the current user and signed in states from the GoogleAuth
- * object.
- */
-var refreshValues = function() {
-  if (auth2){
-    console.log('Refreshing values...');
-
-    googleUser = auth2.currentUser.get();
-
-    document.getElementById('curr-user-cell').innerText =
-      JSON.stringify(googleUser, undefined, 2);
-    document.getElementById('signed-in-cell').innerText =
-      auth2.isSignedIn.get();
-
-    updateGoogleUser();
-  }
+if (auth2.isSignedIn.get()) {
+  var profile = auth2.currentUser.get().getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
 }
