6f18b2f33c3e97c31723daf1945d1738ea388825
Oz Bejerano <ozbejerano@gmail.com>
2018-03-24 15:29:07 -0400

    Oz Front End 3/24 4test20

--- static/main.js
+++ static/main.js
@@ -85,6 +85,7 @@ stk.controller('LeagueController', function($scope){
     };
 });
         stk.controller('NavbarController',['$scope', function($scope){
+            $scope.signOut = signOut;
             $scope.$on('login',function(events,args){
                 var user_profile = googleUser.getBasicProfile();
                 $scope.username = user_profile.getName();
