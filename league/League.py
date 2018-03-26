15994a954fbc1e6a1af455e4444d2a7a567b1af5
Brian <bgm31@scarletmail.rutgers.edu>
2018-03-26 14:48:31 -0400

    League Functions from Brian

--- league/League.py
+++ league/League.py
@@ -1,4 +1,13 @@
 from flask_restful import reqparse, abort, Resource
 
 class League(Resource):
-    pass
+    @staticmethod  # getLeagueInfo
+    def get(cur, LID):
+        """cmd = request.args.get('cmd')
+
+        if cmd == "getLeaguesInfo":
+            cur.execute("SELECT * from leagues where LID like %s;", LID)
+            return cur.fetchone()
+        else:
+            return None
+"""
