6481be333b63fbeb9e4cdcebe748d5b1cdc45f95
Brian <bgm31@scarletmail.rutgers.edu>
2018-03-27 18:01:42 -0400

    League&Leagues functions return json format

--- league/League.py
+++ league/League.py
@@ -1,13 +1,13 @@
 from flask_restful import reqparse, abort, Resource
-
+import json
 class League(Resource):
     @staticmethod  # getLeagueInfo
+    #Getting league info from specified league
     def get(cur, LID):
-        """cmd = request.args.get('cmd')
 
-        if cmd == "getLeaguesInfo":
-            cur.execute("SELECT * from leagues where LID like %s;", LID)
-            return cur.fetchone()
-        else:
-            return None
-"""
+        cur.execute("SELECT uid, pid, startbal, leaguename, description, owneruid, ownerpid, type FROM leagues WHERE lid = %s;", [LID])
+        # league = cur.fetchone()
+        #
+        # return "{'league':" + str(league) + "}"
+        return json.dumps({"Leagues": cur.fetchall()})
+        pass
