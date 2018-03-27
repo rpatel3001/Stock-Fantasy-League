d50727bb9bf9bfb65e95c14138adc13fe7b7e08c
Oz Bejerano <ozbejerano@gmail.com>
2018-03-26 23:24:05 -0400

    Oz 3/26 14

--- static/cookie_manager.js
+++ static/cookie_manager.js
@@ -4,17 +4,6 @@ $http = angular.injector(["ng"]).get("$http");
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     var id_token = googleUser.getAuthResponse().id_token;
-    var scope = angular.element($("#mainNavbar")).scope();
-    scope.$apply(function () {
-        scope.signedIn = true;
-        scope.username = profile.getName();
-        scope.imageurl = profile.getImageUrl();
-    });
-    var scope2 = angular.element($("#logInView")).scope();
-    scope2.$apply(function () {
-        scope2.showLogIn = false;
-    });
-
     var req = {
         method: 'POST',
         url: 'http://stock-fantasy-league.herokuapp.com/api/user',
@@ -33,6 +22,17 @@ function onSignIn(googleUser) {
     }, function loginFailure(response) {
         console.log('Failing to log in!');
     });
+    var scope = angular.element($("#mainNavbar")).scope();
+    scope.$apply(function () {
+        $scope.uid = uid;
+        scope.signedIn = true;
+        scope.username = profile.getName();
+        scope.imageurl = profile.getImageUrl();
+    });
+    /*var scope2 = angular.element($("#logInView")).scope();
+    scope2.$apply(function () {
+        scope2.showLogIn = false;
+    });*/
     /*var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://stock-fantasy-league.herokuapp.com/api/user');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
