1ddb2193fa0e371fbeead148386e4b584fb39b4d
louisyeung <ly178@rutgers.edu>
2018-03-26 21:21:08 -0400

    louis commit - 3/26 -1

--- user/Users.py
+++ user/Users.py
@@ -1,4 +1,5 @@
 from flask_restful import reqparse, abort, Resource
+from flask import flask
 from google.oauth2 import id_token
 from google.auth.transport import requests
 
@@ -13,13 +14,16 @@ class Users(Resource):
 
     @staticmethod   #used to create account
     def post(cur):
-        parser = reqparse.RequestParser()
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('email')
+        # parser.add_argument('username')
+        # parser.add_argument('imageURL')
+        # parser.add_argument('token')
+        # args = parser.parse_args()
 
-        parser.add_argument('email')
-        parser.add_argument('username')
-        parser.add_argument('imageURL')
-        parser.add_argument('token')
-        args = parser.parse_args()
+        args = request.getjson()
+
+
         token = args['token']
         idinfo = id_token.verify_oauth2_token(token, requests.Request())
         logintoken = idinfo['aud']
@@ -29,7 +33,7 @@ class Users(Resource):
         exists = cur.fetchone()
 
         if exists == None:
-            cur.execute("INSERT INTO userprefs (email, username, imageURL, token) VALUES (%s,%s,%s,%s);", (args['email'], args['username'], args['imageURL'], logintoken))
+            cur.execute("INSERT INTO userprefs (email, username, imageurl, token) VALUES (%s,%s,%s,%s);", (args['email'], args['username'], args['imageurl'], logintoken))
             cur.execute("SELECT uid FROM userprefs WHERE token LIKE %s;", (logintoken,))
             return cur.fetchone()
             pass
