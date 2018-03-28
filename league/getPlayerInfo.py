0a228fef8ba479c752d348357f8e32b64ca3b481
louisyeung <ly178@rutgers.edu>
2018-03-28 13:11:36 -0400

    edited function for getPlayerInfoByUID and edited api endpoint

--- league/getPlayerInfo.py
+++ league/getPlayerInfo.py
@@ -1,10 +1,10 @@
 from flask_restful import reqparse, abort, Resource
 
-class getPlayerInfo(Resource):
+class getPlayerInfoByUID(Resource):
 	
 	@staticmethod	# method to grab player UIDs given LID and UID
 	def get(cur, LID, UID):
-		cur.execute("SELECT pid from players WHERE lid = %s AND UID = %s", (LID, UID))
+		cur.execute("SELECT * from players WHERE lid = %s AND UID = %s", (LID, UID))
 		return cur.fetchall()
 		pass
 		
