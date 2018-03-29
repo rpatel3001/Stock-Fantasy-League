9841d7e00e84aa5604eaae9b330146a33115bb04
Louis Yeung <louis@nbp-147-139.nbp.ruw.rutgers.edu>
2018-03-29 19:46:25 -0400

    finished update userprefs and update players

--- player/updatePlayerInfo.py
+++ player/updatePlayerInfo.py
@@ -3,26 +3,28 @@ import json
 
 class updatePlayerInfo(Resource):
 	@staticmethod
-	def POST(cur, PID):
+	def post(cur, PID):
 		parser = reqparse.RequestParser()
 		parser.add_argument('update')
 		args = parser.parse_args()
 		newInfo = json.loads(args['update'])
-		
+		print(type(args['update']))
+		print(type(newInfo))
 		cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'players';")
 		playerColumns = cur.fetchall()
-
+		playerColumns = [x['column_name'] for x in playerColumns]
 		query = ''
 		temp = ''
 		for column in playerColumns:
+			if column == 'holdings' or column == 'notifications' or column == 'pendingorders' or column == 'translog':
+				temp = str(column) + '=' + "'" +  json.dumps((newInfo[column])) + "'"
+				query += temp + ","
+				continue
+			
 			temp = str(column) + '=' + str(newInfo[column])
 			query += temp + ","
 			pass
 		query = query[:-1]
+		cur.execute("UPDATE players SET %s WHERE pid = %s;"% (query, PID))
 
-		cur.execute("UPDATE players SET %s WHERE pid = %s;", (query, PID))
-
-		return "Update Success"
-
-
-		pass
+		return "Success"
--- user/updateUserInfo.py
+++ user/updateUserInfo.py
@@ -2,69 +2,29 @@ from flask_restful import reqparse, abort, Resource
 import json
 
 class updateUserInfo(Resource):
-	
 	@staticmethod
-	def get(cur, UID):
+	def post(cur, UID):
 		parser = reqparse.RequestParser()
 		parser.add_argument('update')
 		args = parser.parse_args()
-		#used for debug
-		# cur.execute("SELECT * FROM userprefs WHERE uid = %s;", ([UID]))
-		# jsonINPUT = cur.fetchall() # =args['update']
-		# for row in jsonINPUT:
-		# print("new information")
-		# print(newInformation)
-		# print("keys")
-		# print(newInformation.keys())
-		# cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'userprefs';")
-		# columns = cur.fetchall()
-		# print("columns")
-		# print(columns)
-		# for column_name in newInformation.keys():
-		# 	toBeUpdated.append(columns)
-		# 	pass
-		jsonstring = args['update']
-		print("jsonstring")
-		print(type(jsonstring))
+		newInfo = json.loads(args['update'])
+		print(type(args['update']))
+		print(type(newInfo))
+		cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'userprefs';")
+		userColumns = cur.fetchall()
+		userColumns = [x['column_name'] for x in userColumns]
+		query = ''
+		temp = ''
+		for column in userColumns:
+			if column == 'messages' or column == 'notifications'
+				temp = str(column) + '=' + "'" +  json.dumps((newInfo[column])) + "'"
+				query += temp + ","
+				continue
+			
+			temp = str(column) + '=' + str(newInfo[column])
+			query += temp + ","
+			pass
+		query = query[:-1]
+		cur.execute("UPDATE userprefs SET %s WHERE uid = %s;"% (query, UID))
 
-		# if (newInformation['uid']) == None:
-		# 	pass
-		# else:
-		# 	# cur.execute("UPDATE userprefs SET uid = %s WHERE uid = %s;", [UID], (newInformation['uid']))
-		# 	print(newInformation['uid'])
-		# 	pass
-
-
-		# for row in newInformation
-		# 	print(row['uid'])
-		# 	cur.execute("UPDATE userprefs SET uid = %s WHERE uid = %s;", [UID], (newInformation['uid']))
-		# 	print(row['lid'])
-		# 	cur.execute("UPDATE userprefs SET lid = %s WHERE uid = %s;", [UID], (newInformation['lid']))
-		# 	print(row['pid'])
-		# 	cur.execute("UPDATE userprefs SET pid = %s WHERE uid = %s;", [UID], (newInformation['pid']))
-		# 	print(row['friends'])
-		# 	cur.execute("UPDATE userprefs SET friends = %s WHERE uid = %s;", [UID], (newInformation['friends']))
-		# 	print(row['email'])
-		# 	cur.execute("UPDATE userprefs SET email = %s WHERE uid = %s;", [UID], (newInformation['email']))
-		# 	print(row['messages'])
-		# 	cur.execute("UPDATE userprefs SET messages= %sWHERE uid = %s;", [UID], (newInformation['messages']))
-		# 	print(row['notifications'])
-		# 	cur.execute("UPDATE userprefs SET notifications= %s WHERE uid = %s;", [UID], (newInformation['notifications']))
-		# 	print(row['username'])
-		# 	cur.execute("UPDATE userprefs SET username = %s WHERE uid = %s;", [UID], (newInformation['username']))
-		# 	print(row['imageurl'])
-		# 	cur.execute("UPDATE userprefs SET imageurl = %s WHERE uid = %s;", [UID], (newInformation['imageurl']))
-		# 	print(row['vip'])
-		# 	cur.execute("UPDATE userprefs SET vip= %s WHERE uid = %s;", [UID], (newInformation['vip']))
-		# 	print(row['token'])
-		# 	cur.execute("UPDATE userprefs SET token = %s WHERE uid = %s;", [UID], (newInformation['token']))
-		# 	print(row['description'])
-		# 	cur.execute("UPDATE userprefs SET description = %s WHERE uid = %s;", [UID], (newInformation['description']))
-
-		# 	pass
-
-
-		return "jsonstring"
-
-		pass
-		
+		return "Success"
