2ef096dcb93b8baeca9543343f97e0093505bed5
Louis Yeung <louis@nbp-147-139.nbp.ruw.rutgers.edu>
2018-03-29 18:27:57 -0400

    update player possibly finished

--- player/updatePlayerInfo.py
+++ player/updatePlayerInfo.py
@@ -7,11 +7,22 @@ class updatePlayerInfo(Resource):
 		parser = reqparse.RequestParser()
 		parser.add_argument('update')
 		args = parser.parse_args()
-
 		newInfo = json.loads(args['update'])
-		print(newinfo)
+		
+		cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'players';")
+		playerColumns = cur.fetchall()
 
-		return "newInfo"
+		query = ''
+		temp = ''
+		for column in playerColumns:
+			temp = str(column) + '=' + str(newInfo[column])
+			query += temp + ","
+			pass
+		query = query[:-1]
+
+		cur.execute("UPDATE players SET %s WHERE pid = %s;", (query, PID))
+
+		return "Update Success"
 
 
 		pass
