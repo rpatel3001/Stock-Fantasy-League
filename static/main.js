d19cfe6b72433f62df81eb985ebc00b795024331
Oz Bejerano <ozbejerano@gmail.com>
2018-03-30 13:39:18 -0400

    40

--- static/main.js
+++ static/main.js
@@ -245,6 +245,20 @@ stk.controller('PlayerController', ['$scope', '$http', '$routeParams', '$route',
                 'sym': $scope.selectedTicker
             }
         };
+        new TradingView.widget({
+            "width": 980,
+            "height": 610,
+            "symbol": $scope.selectedTicker,
+            "interval": "D",
+            "timezone": "Etc/UTC",
+            "theme": "Light",
+            "style": "1",
+            "locale": "en",
+            "toolbar_bg": "#f1f3f6",
+            "enable_publishing": false,
+            "allow_symbol_change": true,
+            "container_id": "tradingview_cd18d"
+        });
         $http(reqPrice).then(function (response) {
             $scope.selectedTicker = stock.symbol;
             $scope.selectedName = stock.name;
