170473ab5222ed8050fc0d9be19e1c34ec23a5f6
Louis Yeung <louis@nbp-145-71.nbp.ruw.rutgers.edu>
2018-04-26 16:23:18 -0400

    bug fix where created league would update ALL of the players

--- user/User.py
+++ user/User.py
@@ -15,15 +15,13 @@ class User(Resource):
         cur.execute("SELECT pid from players where uid=%s;", [UID])  #used to return PID to add into the leagues database
         createdPID = cur.fetchall() #sets equal to dictionary of PIDs
         cur.execute("INSERT INTO leagues (startBal, duration, leagueName, description, ownerUID, ownerPID) VALUES (%s, %s, %s, %s, %s, %s);", (args['startBal'], args['duration'], args['leagueName'], args['description'], UID, createdPID[-1]['pid']))   #creates the league with the user inputs
-        cur.execute("SELECT lid from leagues WHERE ownerUID=%s", [UID])
+        cur.execute("SELECT lid from leagues WHERE ownerUID=%s", [UID]) #gets the leagueID from league table for the created table
         createdLID = cur.fetchall() #sets equal to dictionary of LIDs       
-        cur.execute("UPDATE players SET lid = %s WHERE uid = %s;", (createdLID[-1]['lid'], UID))    #updates players table(add themselves into league)
+        cur.execute("UPDATE players SET lid = %s WHERE pid = %s;", (createdLID[-1]['lid'], createdPID[-1]['pid']))    #updates players table(add themselves into league)
         cur.execute("UPDATE userprefs SET lid = lid || %s WHERE uid = %s;", (createdLID[-1]['lid'], UID))   #updates user table (add themselves into league)
         cur.execute("UPDATE userprefs SET pid = pid || %s WHERE uid = %s;", (createdPID[-1]['pid'], UID))
-
         cur.execute("UPDATE leagues SET uid = uid || %s WHERE lid = %s;", (UID, (createdLID[-1]['lid'])))
         cur.execute("UPDATE leagues SET pid = pid || %s WHERE lid = %s;", (createdPID[-1]['pid'], createdLID[-1]['lid']))
-
         return createdLID
         pass
     
