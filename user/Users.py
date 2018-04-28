454f7e0f8e359718a155e4c82f954f7a820ba462
Louis Yeung <louis@nbp-193-34.nbp.ruw.rutgers.edu>
2018-04-28 18:41:47 -0400

    fixed sending data, orders all data by key

--- user/Users.py
+++ user/Users.py
@@ -9,7 +9,7 @@ class Users(Resource):
 
     @staticmethod   #shows all users in database
     def get(cur):
-        cur.execute("SELECT * FROM userprefs;")
+        cur.execute("SELECT * FROM userprefs ORDER BY pid;")
         return json.dumps({"Users": cur.fetchall()})
 
     @staticmethod   #used to create account
