5e771f184176889bad9a2b061a9de47bab4e0368
Rajan Patel <rpatel3001@gmail.com>
2018-04-29 00:50:01 -0400

    upload questions to db

--- questions/generate_questions.py
+++ questions/generate_questions.py
@@ -26,14 +26,20 @@ cur = db_conn.cursor()
 
 # Question 1
 q1 = "Which of these stock symbols is not real?"
-cur.execute('SELECT symbol FROM stockdata ORDER BY RANDOM() LIMIT 2')
-a11 = cur.fetchone()['symbol']
-a12 = cur.fetchone()['symbol']
+e1 = "One of these stock symbols is not listed on the three major stock exchanges. Stock symbols are unique identifiers for companies on an exchange. "
+cur.execute('SELECT symbol, name FROM stockdata ORDER BY RANDOM() LIMIT 2')
+x = cur.fetchone()
+a11 = x['symbol']
+e11 = "This symbol is for " + x['name']
+x = cur.fetchone()
+a12 = x['symbol']
+e12 = "This symbol is for " + x['name']
 while True:
     a13 = ''.join(random.choices(string.ascii_uppercase, k=random.randint(1, 4)))
     cur.execute("SELECT COUNT(*) FROM stockdata WHERE symbol LIKE '%s'" % a13)
     if cur.fetchone()['count'] == 0:
         break
+e13 = "This symbol does not exist."
 print(q1)
 print(a11)
 print(a12)
@@ -42,6 +48,7 @@ print()
 
 
 q2 = "Which of these stocks has the highest price?"
+e2 = "A stock's price represents its value to investors. Stocks with a higher price represent a higher value to investors. "
 cur.execute('SELECT symbol, price FROM stockdata ORDER BY RANDOM() LIMIT 3')
 u = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey=DEDSQFY460FDRASD&symbol="
 ans = []
@@ -53,8 +60,11 @@ for s in cur.fetchall():
     ans.append((s, float(data['4. close'])))
 ans = sorted(ans, key=lambda k: k[1])
 a21 = ans[0][0]
+e21 = "This stock's price is " + str(ans[0][1])
 a22 = ans[1][0]
+e22 = "This stock's price is " + str(ans[1][1])
 a23 = ans[2][0]
+e23 = "This stock's price is " + str(ans[2][1])
 print(q2)
 print(a21)
 print(a22)
@@ -63,6 +73,7 @@ print()
 
 
 q3 = "Which of these stocks has the highest volume?"
+e3 = "Volume is the number of times a company's stocks are traded each day. More popular stocks generally have a higher volume."
 cur.execute('SELECT symbol FROM stockdata ORDER BY RANDOM() LIMIT 3')
 u = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey=DEDSQFY460FDRASD&symbol="
 ans = []
@@ -74,8 +85,11 @@ for s in cur.fetchall():
     ans.append((s, int(data['5. volume'])))
 ans = sorted(ans, key=lambda k: k[1])
 a31 = ans[0][0]
+e31 = "This stock's volume is " + str(ans[0][1])
 a32 = ans[1][0]
+e32 = "This stock's volume is " + str(ans[1][1])
 a33 = ans[2][0]
+e33 = "This stock's volume is " + str(ans[2][1])
 print(q3)
 print(a31)
 print(a32)
@@ -84,6 +98,7 @@ print()
 
 
 q4 = "Which of these stocks is doing historically well?"
+e4 = "Historical performance can be gauged by comparing a stock's price to its 52 week high and low. A stock near its 52 week high is performing well. "
 cur.execute('SELECT symbol FROM stockdata ORDER BY RANDOM() LIMIT 3')
 u = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey=DEDSQFY460FDRASD&outputsize=full&symbol="
 ans = []
@@ -99,12 +114,15 @@ for s in cur.fetchall():
     for d in data:
         pmax = max(pmax, float(d['2. high']))
         pmin = min(pmin, float(d['3. low']))
-    rp = (price - pmin) / pmax
-    ans.append((s, rp))
+    rp = (price - pmin) / pmin
+    ans.append((s, rp, pmin, pmax))
 ans = sorted(ans, key=lambda k: k[1])
 a41 = ans[0][0]
+e41 = "This stock's 52 week low is " + str(ans[0][2]) + " and its 52 week high is " + str(ans[0][3])
 a42 = ans[1][0]
+e42 = "This stock's 52 week low is " + str(ans[1][2]) + " and its 52 week high is " + str(ans[1][3])
 a43 = ans[2][0]
+e43 = "This stock's 52 week low is " + str(ans[2][2]) + " and its 52 week high is " + str(ans[2][3])
 print(q4)
 print(a41)
 print(a42)
@@ -113,13 +131,18 @@ print()
 
 
 q5 = "Which of these stocks belongs in the sector: "
+e5 = "Stock's can be separated into sectors. All stocks in a sector are related in some way, thus their share prices are related."
 sector = random.choice(("Utilities", "Health Care", "Financials", "Industrials", "Materials"))
 q5 += sector + "?"
 cur.execute("SELECT symbol FROM stockdata WHERE sector LIKE '%s' ORDER BY RANDOM() LIMIT 2" % sector)
 a51 = cur.fetchone()['symbol']
+e51 = "This stock is in the sector: " + sector
 a52 = cur.fetchone()['symbol']
-cur.execute("SELECT symbol FROM stockdata WHERE sector NOT LIKE '%s' ORDER BY RANDOM() LIMIT 1" % sector)
-a53 = cur.fetchone()['symbol']
+e52 = "This stock is in the sector: " + sector
+cur.execute("SELECT symbol, sector FROM stockdata WHERE sector NOT LIKE '%s' ORDER BY RANDOM() LIMIT 1" % sector)
+x = cur.fetchone()
+a53 = x['symbol']
+e53 = "This stock is in the sector: " + x['sector']
 print(q5)
 print(a51)
 print(a52)
@@ -128,6 +151,7 @@ print()
 
 
 q6 = "Which of these stocks is most volatile?"
+e6 = "One measure of a stock's volatility is its 52 week range. A larger range predicts higher volatility."
 cur.execute('SELECT symbol FROM stockdata ORDER BY RANDOM() LIMIT 3')
 u = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey=DEDSQFY460FDRASD&outputsize=full&symbol="
 ans = []
@@ -143,14 +167,27 @@ for s in cur.fetchall():
     for d in data:
         pmax = max(pmax, float(d['2. high']))
         pmin = min(pmin, float(d['3. low']))
-    prange = (pmax - pmin) / pmax
-    ans.append((s, prange))
+    prange = (pmax - pmin) / ((pmax + pmin) / 2)
+    ans.append((s, prange, pmax, pmin))
 ans = sorted(ans, key=lambda k: k[1])
 a61 = ans[0][0]
+e61 = "This stock's 52 week low is " + str(ans[0][2]) + " and its 52 week high is " + str(ans[0][3])
 a62 = ans[1][0]
+e62 = "This stock's 52 week low is " + str(ans[0][2]) + " and its 52 week high is " + str(ans[0][3])
 a63 = ans[2][0]
+e63 = "This stock's 52 week low is " + str(ans[0][2]) + " and its 52 week high is " + str(ans[0][3])
 print(q6)
 print(a61)
 print(a62)
 print(a63)
 print()
+
+
+cur.execute('TRUNCATE TABLE questions RESTART IDENTITY;')
+cur.execute("INSERT INTO questions (question, answer1, answer2, answer3, answerdescription1, answerdescription2, answerdescription3, overalldescription) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (q1, a11, a12, a13, e11, e12, e13, e1))
+cur.execute("INSERT INTO questions (question, answer1, answer2, answer3, answerdescription1, answerdescription2, answerdescription3, overalldescription) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (q2, a21, a22, a23, e21, e22, e23, e2))
+cur.execute("INSERT INTO questions (question, answer1, answer2, answer3, answerdescription1, answerdescription2, answerdescription3, overalldescription) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (q3, a31, a32, a33, e31, e32, e33, e3))
+cur.execute("INSERT INTO questions (question, answer1, answer2, answer3, answerdescription1, answerdescription2, answerdescription3, overalldescription) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (q4, a41, a42, a43, e41, e42, e43, e4))
+cur.execute("INSERT INTO questions (question, answer1, answer2, answer3, answerdescription1, answerdescription2, answerdescription3, overalldescription) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (q5, a51, a52, a53, e51, e52, e53, e5))
+cur.execute("INSERT INTO questions (question, answer1, answer2, answer3, answerdescription1, answerdescription2, answerdescription3, overalldescription) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (q6, a61, a62, a63, e61, e62, e63, e6))
+db_conn.commit()
