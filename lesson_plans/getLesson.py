433d8a7ccb87bf0c511ab63bf02ef26f160c348a
Louis Yeung <louis@nbp-193-34.nbp.ruw.rutgers.edu>
2018-04-30 13:00:30 -0400

    added function to get lessons

--- lesson_plans/getLesson.py
+++ lesson_plans/getLesson.py
@@ -1,7 +1,10 @@
 from flask_restful import reqparse, abort, Resource
 import json
 
-class getLesson(Resource):
+class getlesson(Resource):
 	@staticmethod
 	def get(cur, lessonID):
-		pass
+		lesson = 'lesson_plan_' + str(lessonID)
+		cur.execute("SELECT * from %s;" %lesson)
+		lessonInfo = cur.fetchall();
+		return lessonInfo
