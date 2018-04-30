084feef332c426bd953510fc7265b07c50d2e1bf
Louis Yeung <louis@nbp-193-34.nbp.ruw.rutgers.edu>
2018-04-30 12:10:21 -0400

    edited changes for quiz call, both brians and mine

--- questions/sendQuestion.py
+++ questions/sendQuestion.py
@@ -4,7 +4,7 @@ import json
 class sendQuestion(Resource):
 	@staticmethod
 	def get(cur, QID):
-		cur.execute("SELECT * from questions WHERE qid = %s;", [QID])
+		cur.execute("SELECT * from question WHERE qid = %s;", [QID])
 		questionInfo = cur.fetchone();
 
 		return questionInfo
