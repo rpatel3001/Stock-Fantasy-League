afe8ca8973db2378b1ddad08bc7aafb8aacea36b
Louis Yeung <louis@nbp-147-117.nbp.ruw.rutgers.edu>
2018-04-30 13:23:53 -0400

    lesson plan fix

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ from league import Leagues, League, getPlayerInfoByUID, getLeagueInfoFromArray, 
 from stock_data import StockData, TopStocks, StockSearch
 
 from questions import question, sendQuestion, generate_questions_api, servertime
-
 from lesson_plans import getlesson
 
 
