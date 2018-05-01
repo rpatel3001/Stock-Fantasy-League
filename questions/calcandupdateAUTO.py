631725fd8c48bc83dbbcf05eb40291f94f4fb2c6
Louis Yeung <louis@nbp-143-150.nbp.ruw.rutgers.edu>
2018-05-01 11:10:41 -0400

    updated ranking algorithm

--- questions/calcandupdateAUTO.py
+++ questions/calcandupdateAUTO.py
@@ -11,6 +11,7 @@ class updatepoints(Resource):
 		print("all players: " + str(playerList))
 
 		for player in playerList:
+			print("For player" + str(player))
 			userID = cur.execute("SELECT uid FROM players WHERE pid = %s;", [player,])	#returns the userID from player table which associates with PID
 			userID = cur.fetchone()
 			userID = userID['uid']	#now we have UID of player for this iteration of loop
@@ -22,7 +23,6 @@ class updatepoints(Resource):
 			count = cur.execute("SELECT logcount FROM userprefs WHERE uid = %s;", (userID,))
 			count = cur.fetchone()
 			count = count['logcount']
-			print("log count: " + str(count))
 
 			balance = cur.execute("SELECT availbalance FROM players WHERE pid = %s;", [player,])
 			balance = cur.fetchone()
@@ -32,10 +32,40 @@ class updatepoints(Resource):
 			quizpoints = cur.fetchone()
 			quizpoints = quizpoints['quizpoints']
 
-			score = math.pow(1.5, count)
-			score = score * (balance/1000)
-			score += (0.5 * quizpoints)
+			numberofholdings = cur.execute("SELECT holdings FROM players WHERE pid = %s;", [player,])
+			numberofholdings = cur.fetchone()
+			numberofholdings = numberofholdings['holdings']
+			holdingListNums = [value['numberShares'] for value in numberofholdings] 	#now has the number of shares of each stock
+			holdingListNames = [value['symbol'] for value in numberofholdings] 	#now has the number of shares of each stock
 
+			totalUniqueHoldings = len(numberofholdings) #number of unique shares
+			totalNumHoldings = sum(holdingListNums)	#total number of shares of all stocks
+			shareValues = []
+			for x in range(0, totalUniqueHoldings):
+				cur.execute("SELECT price FROM stockdata WHERE symbol = %s;", (holdingListNames[x],))
+				price = cur.fetchone()
+				price = price['price']
+				shareValues.append(price*holdingListNums[x])
+				pass
+			totalShareValue = sum(shareValues)
+
+			diverse = totalUniqueHoldings * (totalNumHoldings/10)
+
+			# print("log in counter:" + str(count))
+			# print("balance of account:" + str(balance))
+			# print("quiz points:" + str(quizpoints))
+			# print("number of shares:" + str(totalNumHoldings))
+			# print("number of uniques:" + str(totalUniqueHoldings))
+			# print("sharevalues:")
+
+
+
+			score = math.pow(1.5, count)
+			score = score * ((balance+totalShareValue)/1000)
+			score += (0.5 * quizpoints)
+			score += diverse
+
+			print("score: " + str(score))
 			if dailyLog == 1:
 				count += 1
 			else:
