657a7e71f51037caddb12f95c25bf6cd3652eb56
Louis Yeung <louis@nbp-143-150.nbp.ruw.rutgers.edu>
2018-05-01 11:37:51 -0400

    louis1

--- league/getEverythingLeague.py
+++ league/getEverythingLeague.py
@@ -4,5 +4,5 @@ class getEverythingLeague(Resource):
 	'''get all information based on league'''
 	@staticmethod
 	def get(cur, LID):
-		cur.execute("SELECT userprefs.friends, userprefs.email, userprefs.messages, userprefs.notifications, userprefs.username, userprefs.imageurl, userprefs.vip, userprefs.description, players.pid, players.uid, players.lid, players.holdings, players.notifications, players.availbalance, players.pendingorders, players.translog FROM players INNER JOIN userprefs ON players.uid = userprefs.uid WHERE players.lid = %s;", (LID,))
+		cur.execute("SELECT userprefs.friends, userprefs.email, userprefs.messages, userprefs.notifications, userprefs.username, userprefs.imageurl, userprefs.vip, userprefs.description, players.pid, players.uid, players.lid, players.holdings, players.notifications, players.availbalance, players.pendingorders, players.translog, players.reppoints FROM players INNER JOIN userprefs ON players.uid = userprefs.uid WHERE players.lid = %s ORDER BY reppoints DESC;", (LID,))
 		return cur.fetchall()
