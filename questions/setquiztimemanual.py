2700c6f4798867a269df5f604f5309e792aedd18
Louis Yeung <louis@nbp-143-150.nbp.ruw.rutgers.edu>
2018-05-01 09:49:56 -0400

    edited time increment

--- questions/setquiztimemanual.py
+++ questions/setquiztimemanual.py
@@ -8,7 +8,7 @@ class setquiztimemanual(Resource):
 
 		timestamp = time.time()
 
-		timestamp = timestamp + 90	#set 90 seconds later
+		timestamp = timestamp + 15	#set 15 seconds later
 
 
 		cur.execute("UPDATE premade_leagues SET quiztime = %s WHERE startbal = 10000;", (timestamp,))
