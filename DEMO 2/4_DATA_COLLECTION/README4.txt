2cf27dd72ae0d8e66039dce2a2eb5d5baa69d4af
Rajan Patel <rpatel3001@gmail.com>
2018-05-05 20:12:03 -0400

    update readme 4

--- DEMO 2/4_DATA_COLLECTION/README4.txt
+++ DEMO 2/4_DATA_COLLECTION/README4.txt
@@ -9,12 +9,20 @@ https://www.nasdaq.com/screening/companies-by-industry.aspx?&render=download
 
 There is already a copy available in this folder. 
 
-After you have done this, run ‘python update_stock_db.py’.
+After you have done this, you can run ‘python update_stock_db.py’. It will take 
+up to 15 minutes to complete. 
 
 The process of fetching and storing data will fill the database table 
 with stock symbols and associated company names. 
 
 The above process only needs to be done once to initialize the data table. Once 
-the table has been initialized, it will automatically be updated every day at midnight. In order to 
-manually update the table, you can execute ‘python update_stock_db.py’. This 
-process will take several minutes to complete. 
+the table has been initialized, it will automatically be updated every day at 
+midnight. In order to manually update the table, you can execute 
+‘python update_stock_db.py’. This process will take several minutes to complete. 
+
+Further data must be collected when generating questions for the daily game show.
+This includes company fundamentals such as P/E ratios, beta, market capitalization, 
+earnings per share, etc. These values are taken from the AlphaVantage API, as well 
+as other APIs such as Quandl and Intrinio. The values are inserted into randomly 
+chosen questions, and then answers are generated based on these values. The 
+question/answer pairs are then randomized and inserted into the database. 
---
+++ DEMO 2/4_DATA_COLLECTION/update_stock_db.py
@@ -0,0 +1,81 @@
+"""Update the stock database data."""
+
+from urllib.parse import urlparse
+import os
+import psycopg2
+import psycopg2.extras
+from requests import get
+from datetime import date, timedelta
+from time import sleep
+
+
+# calculate the last day that was a weekday
+def weekday():
+    """String representation of the previous weekday."""
+    adate = date.today() - timedelta(days=1)
+    while adate.weekday() > 4:
+        adate -= timedelta(days=1)
+    return adate.strftime('%Y-%m-%d')
+
+
+# split a list into sized chunks
+def chunk(n, iterable):
+    """Util to iterate over a list in chunks."""
+    args = [iter(iterable)] * n
+    return zip(*args)
+
+
+# init postgresql connection
+try:
+    url = urlparse(os.environ["DATABASE_URL"])
+except:
+    url = urlparse("postgres://xqeuyuquhktxoq:c7869c5a14cb7f47eea6a586dbc23ffe5ee8522abfb2c3c4b2b0785db64b5916@ec2-54-243-239-66.compute-1.amazonaws.com:5432/den0hekga678pn")
+db_conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port,
+    cursor_factory=psycopg2.extras.RealDictCursor
+)
+
+u = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey=DEDSQFY460FDRASD&symbol="
+
+# select all the stock symbols in the database
+i = 0
+with db_conn.cursor() as cur:
+    cur.execute("SELECT symbol FROM stockdata;")
+    fullsyms = [x['symbol'] for x in cur.fetchall()]
+
+for sym in fullsyms:
+    # print the current index to monitor progress
+    print(i, end='\r')
+    i += 1
+    # sleep to avoid hitting API rate limits
+    sleep(.5)
+    data = {'Information': 0}
+    while True:
+        data = get(u + sym).json()
+        # if we hit the rate limit anyway, sleep a long time then try again
+        if 'Information' not in data.keys():
+            break
+        print(i)
+        sleep(30)
+    # if the symbol doesn't exist then skip it
+    try:
+        data = data['Time Series (Daily)']
+        data = data[list(data.keys())[0]]
+    except KeyError:
+        print(sym)
+        print(data)
+        print()
+        continue
+    strs = "('%s', %s, %s)" % (sym, data['4. close'], data['5. volume'])
+    with db_conn:
+        with db_conn.cursor() as cur:
+            # insert the new stock data into the database
+            cur.execute("INSERT INTO stockdata (symbol, price, volume) "
+                        "VALUES %s "
+                        "ON CONFLICT(symbol) DO UPDATE "
+                        "SET price = EXCLUDED.price,"
+                        "volume = EXCLUDED.volume;" % strs)
